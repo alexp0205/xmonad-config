a234d0f35847efb44fc3356c498597aa6f988ccc
Alex <alexp0205@gmail.com>
2018-08-02 11:32:20 +0530

    Polybar on both monitors

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,8 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1 and bar2
-polybar bar1 &
-polybar bar2 &
+# Launch left monitor and right monitor bars
+polybar leftbar &
+monitorCount=$(polybar --list-monitors | wc -l)
+if [ "$monitorCount" -gt "1" ]
+then
+  polybar rightbar &
+fi
 
 echo "Bars launched..."
